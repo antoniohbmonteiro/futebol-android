38880fe4bc0d30aae37e7f3deb734560b8b23fa7
Antonio <antoniohbmonteiro@gmail.com>
2023-04-03 08:22:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/antoniomonteiro/futebol/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.antoniomonteiro.futebol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.antoniomonteiro.futebol.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
